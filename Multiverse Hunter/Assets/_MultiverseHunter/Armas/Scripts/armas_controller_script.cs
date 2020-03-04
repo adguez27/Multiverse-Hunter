308249687d55e411deae5b48429f2834d0c5a470
Alex <55707078+adguez27@users.noreply.github.com>
2020-03-04 11:35:12 +0100

    Controles y ataque básico

---
+++ Multiverse Hunter/Assets/_MultiverseHunter/Armas/Scripts/armas_controller_script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class armas_controller_script : MonoBehaviour
+{
+    public bool isFiring;
+    public GameObject bullet;
+    public float BulletSpeed = 30;
+    public float timeBetweenShots;
+    private float shotCounter;
+    public Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shotCounter -= Time.deltaTime;
+        if (isFiring == true)
+        {
+            
+            if (shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                GameObject newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as GameObject;
+                balas_controller_script controller = (balas_controller_script)newBullet.GetComponent("balas_controller_script");
+                controller.speed = BulletSpeed;
+            }
+            
+        }
+    }
+}
---
+++ Multiverse Hunter/Assets/_MultiverseHunter/Jugador/jugador_apuntar_script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jugador_apuntar_script : MonoBehaviour
+{
+    public armas_controller_script theGun;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if(groundPlane.Raycast(cameraRay,out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+            transform.LookAt(new Vector3(pointToLook.x,transform.position.y,pointToLook.z));
+        }
+        if (Input.GetMouseButtonDown(0))
+            theGun.isFiring = true;
+
+        if (Input.GetMouseButtonUp(0))
+            theGun.isFiring = false;
+    }
+}
